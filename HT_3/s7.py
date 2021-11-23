15cdbfeea1121f77804f21d5478f01fbf79bbd4f
SergeyBerezhnoy <cs070284bsn@gmail.com>
2021-11-23 17:12:57 +0200

    homework3 - 7 remark

--- HT_3/s7.py
+++ HT_3/s7.py
@@ -5,36 +5,33 @@
 
 def calc(digit1,oper,digit2):
     if oper == '+':
-        print(int(digit1) + int(digit2))
+        print(float(digit1) + float(digit2))
     if oper == '-':
-        print(int(digit1) - int(digit2))
+        print(float(digit1) - float(digit2))
     if oper == '*':
-        print(int(digit1) * int(digit2))
+        print(float(digit1) * float(digit2))
     if oper == '/':
-        print(int(digit1) / int(digit2))
+        print(float(digit1) / float(digit2))
 
 
 s = input("calculate:")
-d1 = int()
-d2 = int()
+d1 = float()
+d2 = float()
 o = ''
 if '+' in s:
     d1, d2 = s.split('+')
     o = '+'
+elif '-' in s:
+    d1, d2 = s.split('-')
+    o = '-'
+elif '*' in s:
+    d1, d2 = s.split('*')
+    o = '*'
+elif '/' in s:
+    d1, d2 = s.split('/')
+    o = '/'
 else:
-    if '-' in s:
-        d1, d2 = s.split('-')
-        o = '-'
-    else:
-        if '*' in s:
-            d1, d2 = s.split('*')
-            o = '*'
-        else:
-            if '/' in s:
-                d1, d2 = s.split('/')
-                o = '/'
-            else:
-                print("nothing to do, sorry")
+    print("nothing to do, sorry")
 if len(o):
     try:
         calc(d1, o, d2)
