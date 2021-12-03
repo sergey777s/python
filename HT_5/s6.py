61cfdce046f587f3c145635523ef277254f47ae2
SergeyBerezhnoy <cs070284bsn@gmail.com>
2021-12-03 10:37:57 +0200

    remarks

--- HT_5/s6.py
+++ HT_5/s6.py
@@ -7,6 +7,14 @@
 
 
 def srange(start, stop=0, step=1):
+    if step == 0:
+        raise ValueError("range() arg 3 must not be zero")
+    if start > stop and step > 0:
+        return
+    if start < stop and step > 0:
+        while start < stop:
+            yield start
+            start += step
     if stop == 0:
         stop = start
         start = 0
@@ -14,7 +22,9 @@ def srange(start, stop=0, step=1):
             yield start
             start = start + step
     if step < 0:
+        if start <= stop:
+            return
         start, stop = stop, start
-        while start > stop:
-            yield start
-            start = start + step
+        while start < stop:
+            yield stop
+            stop = stop - abs(step)
