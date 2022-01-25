dd23431b735c6f8b86eb62c1b8faeec384ec8725
SergeyBerezhnoy <cs070284bsn@gmail.com>
2022-01-25 18:32:49 +0200

    remarks

--- HT_17/s1.py
+++ HT_17/s1.py
@@ -18,15 +18,20 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 chr = Chrome(executable_path="./chromedriver", service=Service(ChromeDriverManager().install()))
 chr.get(
     "https://docs.google.com/forms/d/e/1FAIpQLScLhHgD5pMnwxl8JyRfXXsJekF8_pDG36XtSEwaGsFdU2egyw/viewform?usp=sf_link")
-nameInput = chr.find_element(By.CSS_SELECTOR, ".quantumWizTextinputPaperinputInput.exportInput")
+nameInput = WebDriverWait(chr, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, ".quantumWizTextinputPaperinputInput.exportInput")))
 nameInput.click()
 nameInput.send_keys("Sergey Berezhnoy")
 chr.save_screenshot("commit.jpg")
-chr.find_element(
-    By.CSS_SELECTOR, ".appsMaterialWizButtonPaperbuttonLabel.quantumWizButtonPaperbuttonLabel.exportLabel").click()
+confirmButton = WebDriverWait(chr, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR,
+                                ".appsMaterialWizButtonPaperbuttonLabel.quantumWizButtonPaperbuttonLabel.exportLabel")))
+confirmButton.click()
 chr.save_screenshot("push.jpg")
 chr.close()
